d97ae268ea7d5bf3c82701f3d229695b65524ee8
NoaRicky <480582562@qq.com>
2019-01-04 23:09:37 +0800

    init pygame

---
+++ astar/main.py
@@ -0,0 +1,28 @@
+import pygame
+import random
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("my_game")
+clock = pygame.time.Clock()
+
+# Game loop
+running = True
+while running:
+    # Process input (event)
+    # Update
+    # Draw / render
+    screen.fill(BLACK)
